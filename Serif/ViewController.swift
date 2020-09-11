7f03ce5859ffb3299d5f82b71ef09a1ac207f7ab
prosciutto0509 <cqcfoxhoundrexrayyy@yahoo.co.jp>
2020-09-11 12:09:00 +0900

    Initial Commit

---
+++ Serif/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Serif
+//
+//  Created by 安田 悠麿 on 2020/09/11.
+//  Copyright © 2020 安田 悠麿. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
