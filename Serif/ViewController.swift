cc21738f62d25ca874db1e33008798b23f75505f
prosciutto0509 <cqcfoxhoundrexrayyy@yahoo.co.jp>
2020-09-12 14:14:53 +0900

    AVFoundationCreate

--- Serif/ViewController.swift
+++ Serif/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+var selif = Selif()
+  var speechSynthesizer : AVSpeechSynthesizer!
+  @IBOutlet weak var first: UIButton!
+  
 
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    first.setTitle(selif.word, for: .normal)
+    
+    
   }
-
-
+  @IBAction func button(_ sender: Any) {
+    self.speechSynthesizer = AVSpeechSynthesizer()
+     let utterance = AVSpeechUtterance(string: selif.word!)// 読み上げる文字
+    utterance.voice = AVSpeechSynthesisVoice(language: "ja-JP")
+    utterance.rate = 0.5;  //読み上げ速度
+    utterance.pitchMultiplier = 0.5; //声のピッチ
+     utterance.preUtteranceDelay = 0.2;//読み上げるまでのため
+    
+    self.speechSynthesizer.speak(utterance)
+  }
+  
+  @IBAction func reset(_ sender: Any) {
+    self.selif = Selif()
+    first.setTitle(selif.word, for: .normal)
+    
+  }
+  
 }
+
+
+class Selif{
+  var word:String? = nil
+  init() {
+    let list: [String] = ["はもん", "みちが", "きょうは"]
+    let element = list.randomElement()
+    self.word = element!
+  }
+}
+
 
